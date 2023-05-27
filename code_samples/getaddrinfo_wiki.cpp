4a3e07ed5fe4168ad52008542f0af092377a1894
Axel Brisse <axel.brisse@gmail.com>
2023-05-27 09:05:25 +0200

    code samples for authorizeed functions

---
+++ code_samples/getaddrinfo_wiki.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+#define MAX_HOST 1024
+
+int main(void) {
+	struct addrinfo* result;
+	int error = getaddrinfo("chess.com", NULL, NULL, &result);
+	if (error) {
+		if (error == EAI_SYSTEM)
+			perror("getaddrinfo");
+		else
+			fprintf(stderr, "error in getaddrinfo: %s\n", gai_strerror(error));
+		return EXIT_FAILURE;
+	}
+
+	for (struct addrinfo* cur = result; cur != NULL; cur = cur->ai_next) {
+		char hostname[MAX_HOST];
+		if (getnameinfo(cur->ai_addr, cur->ai_addrlen, hostname, MAX_HOST, NULL, 0, 0))
+			fprintf(stderr, "error in getnameinfo: %s\n", gai_strerror(error));
+		else
+			printf("hostname: %s\n", hostname);
+	}
+
+	freeaddrinfo(result);
+	return EXIT_SUCCESS;
+}
---
+++ code_samples/somaxconn.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <sys/socket.h>
+
+int main() { std::cout << "SOMAXCONN: " << SOMAXCONN << std::endl; }
