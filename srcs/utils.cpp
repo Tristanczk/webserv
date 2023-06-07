9f26137a5edaca7a002a36b139c1e4fb2a82905c
norxondor_gorgonax <axel.brisse@gmail.com>
2023-06-07 09:32:07 +0200

    more leet getIpValue

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -1,7 +1,6 @@
 #include "../includes/webserv.hpp"
 
 bool getIpValue(std::string ip, uint32_t& res) {
-	size_t idx;
 	if (ip == "localhost") {
 		res = htonl(INADDR_LOOPBACK);
 		return true;
@@ -10,23 +9,24 @@ bool getIpValue(std::string ip, uint32_t& res) {
 		res = htonl(INADDR_ANY);
 		return true;
 	}
-	int count = 0;
+	size_t idx;
+	int dots = 0;
 	while ((idx = ip.find('.')) != std::string::npos) {
 		ip.replace(idx, 1, 1, ' ');
-		count++;
+		++dots;
 	}
-	if (count != 3)
+	if (dots != 3)
 		return false;
 	std::istringstream iss(ip);
 	std::string check;
 	int val[4];
 	if (!(iss >> val[0] >> val[1] >> val[2] >> val[3]))
 		return false;
-	for (int i = 0; i < 4; i++) {
+	res = 0;
+	for (int i = 0; i < 4; ++i) {
 		if (val[i] < 0 || val[i] > 255)
 			return false;
-		res += val[i] << (8 * (3 - i));
-		// TODO res = res << 8 | val[i]
+		res = res << 8 | val[i];
 	}
 	if (iss >> check)
 		return false;
