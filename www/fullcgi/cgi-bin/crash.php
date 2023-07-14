9d6d7ba7e177098c66e9202c159dd28a9eda53ec
Axel Brisse <axel.brisse@gmail.com>
2023-07-15 01:47:49 +0200

    418 and better fullcgi index

---
+++ www/fullcgi/cgi-bin/crash.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php-cgi
+
+<?php
+header("Content-Type: text/html");
+
+echo "<!DOCTYPE html>";
+echo '<html lang="en">';
+echo "<head><title>crash</title></head>";
+echo "<body>";
+echo "</body>";
+echo "</html>";
+
+lol
+
+?>
--- www/fullcgi/cgi-bin/teapot.php
+++ www/fullcgi/cgi-bin/teapot.php
@@ -2,6 +2,7 @@
 
 <?php
 header("Content-Type: text/html");
+header("Status: 418 I'm a teapot");
 
 echo "<!DOCTYPE html>";
 echo '<html lang="en">';
@@ -17,7 +18,5 @@ foreach ($_SERVER as $key => $value) {
 echo "</table>";
 echo "</body>";
 echo "</html>";
-
-lol
 
 ?>
