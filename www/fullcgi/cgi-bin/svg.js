9e9acea4b205bfba1ca9d0ae152c77386c613015
Axel Brisse <axel.brisse@gmail.com>
2023-07-15 01:57:20 +0200

    bungee shade

--- www/fullcgi/cgi-bin/svg.js
+++ www/fullcgi/cgi-bin/svg.js
@@ -7,6 +7,9 @@ console.log(`Content-Type: image/svg+xml
 
 <svg width="100%" height="100%" style="background-color: #596886;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 600">
     <defs>
+        <style type="text/css">
+			@import url('https://fonts.googleapis.com/css2?family=Bungee+Shade&amp;display=swap');
+        </style>
         <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
             <stop offset="0%" style="stop-color:rgb(255,180,20);stop-opacity:1" />
             <stop offset="100%" style="stop-color:rgb(255,20,180);stop-opacity:1" />
@@ -16,13 +19,12 @@ console.log(`Content-Type: image/svg+xml
         </filter>
     </defs>
     <g transform="rotate(-15 500 300)">
-        <text x="50%" y="40%" alignment-baseline="middle" text-anchor="middle" font-family="Verdana" font-size="120" filter="url(#3d)">
+        <text x="50%" y="40%" alignment-baseline="middle" text-anchor="middle" font-family="'Bungee Shade', cursive" font-size="120" filter="url(#3d)">
             Hello ${name}
         </text>
     </g>
     <style>
         text {
-            font-weight: bold;
             fill: url(#gradient);
             stroke: #fff;
             stroke-width: 2px;
